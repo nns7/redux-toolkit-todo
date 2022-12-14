76abe943554d9e0163e34add55c7262cdd7805ea
nns7 <nns7snn@gmail.com>
2022-12-14 16:43:15 +0900

    :sparkles: Sliceの定義。

---
+++ src/features/todos/todoSlice.ts
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { Todo } from "../../common/todo.type";
+
+const state = {
+  todos: [
+    {
+      id: 1,
+      title: "テスト1",
+      content: "テスト1の内容",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "テスト2",
+      content: "テスト2の内容",
+      isCompleted: false,
+    },
+  ] as Todo[],
+};
+
+export const todoSlice = createSlice({
+  name: "todoSlice",
+  initialState: state,
+  reducers: {},
+});
